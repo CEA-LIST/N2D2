3c2899761f0779823683ce132355948b30631a49
David BRIAND - DRT/LIST/DACLE/LCE - 2015/06/03 <david.briand@cea.fr>
2019-04-09 10:46:49 +0200

    Fix issue in AnchorCellGenerator

--- src/Generator/AnchorCellGenerator.cpp
+++ src/Generator/AnchorCellGenerator.cpp
@@ -18,6 +18,7 @@
     knowledge of the CeCILL-C license and that you accept its terms.
 */
 
+#include "StimuliProvider.hpp"
 #include "Generator/AnchorCellGenerator.hpp"
 
 N2D2::Registrar<N2D2::CellGenerator>
