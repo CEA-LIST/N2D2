CEA-LIST/N2D2

commit 80f788c8671b9f4b7b6caabd3ad6c5e19080332c

Improved Target

src/Target/TargetROIs.cpp
@@ -481,7 +481,12 @@ void N2D2::TargetROIs::logEstimatedLabels(const std::string& dirName) const
         const std::string imgFile
             = mStimuliProvider->getDatabase().getStimulusName(id);
         const std::string baseName = Utils::baseName(imgFile);
-        const std::string fileName = dirPath + "/" + baseName;
+        const std::string fileBaseName = Utils::fileBaseName(baseName);
+        const std::string fileExtension
+            = (!((std::string)mImageLogFormat).empty())
+                ? mImageLogFormat : Utils::fileExtension(baseName);
+        const std::string fileName = dirPath + "/" + fileBaseName
+                                        + "." + fileExtension;
 
         // Draw image
         if (!cv::imwrite(fileName, drawEstimatedLabels(batchPos)))
@@ -491,8 +496,7 @@ void N2D2::TargetROIs::logEstimatedLabels(const std::string& dirName) const
         const std::vector<DetectedBB>& detectedBB = mDetectedBB[batchPos];
 
         if (!detectedBB.empty()) {
-            const std::string logFileName = Utils::fileBaseName(fileName)
-                                            + ".log";
+            const std::string logFileName = fileBaseName + ".log";
             std::ofstream roisData(logFileName.c_str());
 
             if (!roisData.good())

include/Target/Target.hpp
@@ -146,6 +146,8 @@ protected:
     Parameter<int> mMaskedLabel;
     /// Threshold for single output (binary classification). Default is 0.5.
     Parameter<double> mBinaryThreshold;
+    /// If left empty, use the database image origin format
+    Parameter<std::string> mImageLogFormat;
 
     const std::string mName;
     const std::shared_ptr<Cell> mCell;

src/Target/Target.cpp
@@ -42,6 +42,7 @@ N2D2::Target::Target(const std::string& name,
       mLabelsHueOffset(this, "LabelsHueOffset", 0),
       mMaskedLabel(this, "MaskedLabel", -1),
       mBinaryThreshold(this, "BinaryThreshold", 0.5),
+      mImageLogFormat(this, "ImageLogFormat", "jpg"),
       mName(name),
       mCell(cell),
       mStimuliProvider(sp),
@@ -403,10 +404,12 @@ void N2D2::Target::process(Database::StimuliSet set)
             Tensor<Float_T> estimatedLabelsValue
                 = mEstimatedLabelsValue[batchPos];
 
-            if (mTargetTopN > value.dimZ())
+            if (mTargetTopN > value.dimZ()) {
+#pragma omp critical(Target__process)
                 throw std::runtime_error("Target::process(): target 'TopN' "
                                          "parameter must be <= to the network "
                                          "output size");
+            }
 
             if (target.size() == 1) {
                 if (target(0) >= 0) {
@@ -566,7 +569,9 @@ void N2D2::Target::logEstimatedLabels(const std::string& dirName) const
                 = mStimuliProvider->getDatabase().getStimulusName(id);
             const std::string baseName = Utils::baseName(imgFile);
             const std::string fileBaseName = Utils::fileBaseName(baseName);
-            const std::string fileExtension = Utils::fileExtension(baseName);
+            const std::string fileExtension
+                = (!((std::string)mImageLogFormat).empty())
+                    ? mImageLogFormat : Utils::fileExtension(baseName);
 
             // Input image
             cv::Mat inputImg = (cv::Mat)mStimuliProvider->getData(0, batchPos);
@@ -576,8 +581,10 @@ void N2D2::Target::logEstimatedLabels(const std::string& dirName) const
             std::string fileName = dirPath + "/" + fileBaseName + "_target."
                                    + fileExtension;
 
-            if (!cv::imwrite(fileName, inputImg8U))
+            if (!cv::imwrite(fileName, inputImg8U)) {
+#pragma omp critical(Target__logEstimatedLabels)
                 throw std::runtime_error("Unable to write image: " + fileName);
+            }
 
             // Output image
             const cv::Mat outputImg = (cv::Mat)values[batchPos][0];
@@ -587,8 +594,10 @@ void N2D2::Target::logEstimatedLabels(const std::string& dirName) const
             fileName = dirPath + "/" + fileBaseName + "_estimated."
                        + fileExtension;
 
-            if (!cv::imwrite(fileName, outputImg8U))
+            if (!cv::imwrite(fileName, outputImg8U)) {
+#pragma omp critical(Target__logEstimatedLabels)
                 throw std::runtime_error("Unable to write image: " + fileName);
+            }
         }
 
         return;
@@ -652,7 +661,9 @@ void N2D2::Target::logEstimatedLabels(const std::string& dirName) const
             = mStimuliProvider->getDatabase().getStimulusName(id);
         const std::string baseName = Utils::baseName(imgFile);
         const std::string fileBaseName = Utils::fileBaseName(baseName);
-        const std::string fileExtension = Utils::fileExtension(baseName);
+        const std::string fileExtension
+            = (!((std::string)mImageLogFormat).empty())
+                ? mImageLogFormat : Utils::fileExtension(baseName);
 
         // Input image
         cv::Mat inputImg = (cv::Mat)mStimuliProvider->getData(0, batchPos);
@@ -697,8 +708,10 @@ void N2D2::Target::logEstimatedLabels(const std::string& dirName) const
         std::string fileName = dirPath + "/" + fileBaseName + "_target."
                                + fileExtension;
 
-        if (!cv::imwrite(fileName, imgBlended))
+        if (!cv::imwrite(fileName, imgBlended)) {
+#pragma omp critical(Target__logEstimatedLabels)
             throw std::runtime_error("Unable to write image: " + fileName);
+        }
 
         // Estimated image
 #if CV_MAJOR_VERSION >= 3
@@ -720,8 +733,10 @@ void N2D2::Target::logEstimatedLabels(const std::string& dirName) const
 
         fileName = dirPath + "/" + fileBaseName + "_estimated." + fileExtension;
 
-        if (!cv::imwrite(fileName, imgBlended))
+        if (!cv::imwrite(fileName, imgBlended)) {
+#pragma omp critical(Target__logEstimatedLabels)
             throw std::runtime_error("Unable to write image: " + fileName);
+        }
     }
 }
 

src/Target/TargetBBox.cpp
@@ -263,7 +263,12 @@ void N2D2::TargetBBox::logEstimatedLabels(const std::string& dirName) const
         const std::string imgFile
             = mStimuliProvider->getDatabase().getStimulusName(id);
         const std::string baseName = Utils::baseName(imgFile);
-        const std::string fileName = dirPath + "/" + baseName;
+        const std::string fileBaseName = Utils::fileBaseName(baseName);
+        const std::string fileExtension
+            = (!((std::string)mImageLogFormat).empty())
+                ? mImageLogFormat : Utils::fileExtension(baseName);
+        const std::string fileName = dirPath + "/" + fileBaseName
+                                        + "." + fileExtension;
 
         // Draw image
 

src/Target/TargetRP.cpp
@@ -417,7 +417,12 @@ void N2D2::TargetRP::logEstimatedLabels(const std::string& dirName) const
         const std::string imgFile
             = mStimuliProvider->getDatabase().getStimulusName(id);
         const std::string baseName = Utils::baseName(imgFile);
-        const std::string fileName = dirPath + "/" + baseName;
+        const std::string fileBaseName = Utils::fileBaseName(baseName);
+        const std::string fileExtension
+            = (!((std::string)mImageLogFormat).empty())
+                ? mImageLogFormat : Utils::fileExtension(baseName);
+        const std::string fileName = dirPath + "/" + fileBaseName
+                                        + "." + fileExtension;
 
         // Draw image
         if (!cv::imwrite(fileName, drawEstimatedLabels(batchPos)))

commit d510011cac0a78a07a58831249a2b3d513a1ca2f

Corrected bug introduced in 80f788c8671b9f4b7b6caabd3ad6c5e19080332c

src/Target/TargetROIs.cpp
@@ -502,7 +502,8 @@ void N2D2::TargetROIs::logEstimatedLabels(const std::string& dirName) const
         const std::vector<DetectedBB>& detectedBB = mDetectedBB[batchPos];
 
         if (!detectedBB.empty()) {
-            const std::string logFileName = fileBaseName + ".log";
+            const std::string logFileName = Utils::fileBaseName(fileName)
+                + ".log";
             std::ofstream roisData(logFileName.c_str());
 
             if (!roisData.good())