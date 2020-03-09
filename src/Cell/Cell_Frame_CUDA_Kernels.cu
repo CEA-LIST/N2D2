2d9060ac2136981c80c2fdef545433c01e8e296f
Olivier BICHLER - DRT/LIST/DACLE/SCSN/LCE <olivier.bichler@cea.fr>
2020-03-09 11:30:06 +0100

    Fixed CUDA error

--- src/Cell/Cell_Frame_CUDA_Kernels.cu
+++ src/Cell/Cell_Frame_CUDA_Kernels.cu
@@ -375,13 +375,14 @@ void cudaHApplyLoss_kernel(__half* lossMem,
                         __float2half(
                             abs(__half2float(diffInputs[outputsIdx]))));
                     lossMem[outputsIdx] = __hmul(error, error);
+                    diffInputs[outputsIdx] = error;
 #else
                     const float error = (__half2float(val)
                         - __half2float(outputs[outputsIdx]))
                             * abs(__half2float(diffInputs[outputsIdx]));
                     lossMem[outputsIdx] = __float2half(error * error);
+                    diffInputs[outputsIdx] = __float2half(error);
 #endif
-                    diffInputs[outputsIdx] = error;
                 }
                 else
                     lossMem[outputsIdx] = __float2half(0.0f);
