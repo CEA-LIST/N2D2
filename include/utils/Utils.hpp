CEA-LIST/N2D2

commit 77fce58ec7c949bbf5b64dc4fd3c6ff9da1448a3

Move the implicit normalization done on integers stimuli from the Tensor(const cv::Mat&, bool) constructor into a RangeAffineTransformation.

src/Database/Database.cpp
@@ -42,6 +42,10 @@ N2D2::Database::Database(bool loadDataInMemory)
     // ctor
 }
 
+bool N2D2::Database::empty() const {
+    return mStimuli.empty();
+}
+
 void N2D2::Database::loadROIs(const std::string& fileName,
                               const std::string& relPath,
                               bool noImageSize)
@@ -1704,6 +1708,28 @@ int N2D2::Database::labelID(const std::string& labelName)
         mLabelsName.push_back(labelName);
         return (mLabelsName.size() - 1);
     }
+}
+
+int N2D2::Database::getStimuliDepth() {
+    if (mStimuliDepth == -1) {
+        if(mStimuli.empty()) {
+            throw std::runtime_error("Can't get the stimuli depth of an empty database.");
+        }
+
+        std::string fileExtension = Utils::fileExtension(mStimuli[0].name);
+        std::transform(fileExtension.begin(), fileExtension.end(),
+                       fileExtension.begin(), ::tolower);
+
+        auto dataFile = Registrar<DataFile>::create(fileExtension)();
+        mStimuliDepth = dataFile->read(mStimuli[0].name).depth();
+
+        std::cout << Utils::cnotice << "Notice: stimuli depth is "
+                    << Utils::cvMatDepthToString(mStimuliDepth)
+                    << " (according to database first stimulus)"
+                    << Utils::cdef << std::endl;
+    }
+
+    return mStimuliDepth;
 }
 
 cv::Mat N2D2::Database::loadStimulusData(StimulusID id)

src/StimuliProvider.cpp
@@ -20,6 +20,7 @@
 
 #include "StimuliProvider.hpp"
 #include "Solver/SGDSolver_Kernels.hpp"
+#include "Transformation/RangeAffineTransformation.hpp"
 #include "utils/BinaryCvMat.hpp"
 #include "utils/Gnuplot.hpp"
 #include "utils/GraphViz.hpp"
@@ -302,6 +303,50 @@ void N2D2::StimuliProvider::addTopTransformation(const CompositeTransformation& 
     else {
         addOnTheFlyTransformation(transformation, setMask);
     }
+}
+
+bool N2D2::StimuliProvider::normalizeIntegersStimuli(int envCvDepth) {
+    bool normalizationOccured = false;
+
+    for(Database::StimuliSet stimuliSet: mDatabase.getStimuliSets(Database::All)) {
+        int depthSet = envCvDepth;
+        depthSet = getTransformation(stimuliSet).getOutputsDepth(depthSet);
+        depthSet = getOnTheFlyTransformation(stimuliSet).getOutputsDepth(depthSet);
+
+
+        if(mChannelsTransformations.empty()) {
+            const double depthUnityValue = Utils::cvMatDepthUnityValue(depthSet);
+            if(depthUnityValue != 1.0) {
+                normalizationOccured = true;
+
+                addOnTheFlyTransformation(
+                    RangeAffineTransformation(RangeAffineTransformation::Divides, depthUnityValue), 
+                    mDatabase.getStimuliSetMask(stimuliSet)
+                );
+            }
+        }
+        else {
+            for(std::size_t ch = 0; ch < mChannelsTransformations.size(); ch++) {
+                int chDepthSet = depthSet;
+                chDepthSet = mChannelsTransformations[ch](stimuliSet).cacheable.getOutputsDepth(chDepthSet);
+                chDepthSet = mChannelsTransformations[ch](stimuliSet).onTheFly.getOutputsDepth(chDepthSet);
+
+
+                const double depthUnityValue = Utils::cvMatDepthUnityValue(chDepthSet);
+                if(depthUnityValue != 1.0) {
+                    normalizationOccured = true;
+
+                    addChannelOnTheFlyTransformation(
+                        ch,
+                        RangeAffineTransformation(RangeAffineTransformation::Divides, depthUnityValue), 
+                        mDatabase.getStimuliSetMask(stimuliSet)
+                    );
+                }
+            }
+        }
+    }
+
+    return normalizationOccured;
 }
 
 void N2D2::StimuliProvider::logTransformations(const std::string& fileName)

exec/n2d2.cpp
@@ -1759,6 +1759,22 @@ int main(int argc, char* argv[]) try
                               Database::TestOnly);
     }
 
+    /**
+     * Historically N2D2 normalized integers stimuli in the [0.0;1.0] or [-1.0;1.0] range, 
+     * depending on the signess, when loading integer stimuli inside a floating-point Tensor.
+     * 
+     * Keep this implicit conversion for backward compatibility. 
+     * TODO Make plans to eventually remove it.
+     */
+    if(deepNet->getDatabase() != nullptr && !deepNet->getDatabase()->empty()) {
+        const int stimuliCvDepth = deepNet->getDatabase()->getStimuliDepth();
+        if(deepNet->getStimuliProvider()->normalizeIntegersStimuli(stimuliCvDepth)) {
+            std::cout << Utils::cnotice << "Notice: normalizing the stimuli in the [0;1] range." 
+                      << Utils::cdef << std::endl;
+        }
+    }
+
+
     if (!opt.saveTestSet.empty()) {
         StimuliProvider& sp = *deepNet->getStimuliProvider();
 

include/Database/Database.hpp
@@ -224,6 +224,7 @@ public:
      * @return Number of stimuli
     */
     inline unsigned int getNbStimuli() const;
+    bool empty() const;
 
     /**
      * Returns the number of stimuli in one stimuli set.
@@ -291,6 +292,7 @@ public:
                                          <std::string>& names) const;
     std::vector<int> getMatchingLabelsIDs(const std::string& labelMask) const;
     inline const std::string& getLabelName(int label) const;
+    int getStimuliDepth();
     cv::Mat getStimulusData(StimulusID id);
     inline cv::Mat getStimulusData(StimuliSet set, unsigned int index);
     cv::Mat getStimulusLabelsData(StimulusID id);

include/StimuliProvider.hpp
@@ -149,6 +149,13 @@ public:
     void addTopTransformation(const CompositeTransformation& transformation,
                               Database::StimuliSetMask setMask = Database::All);
 
+    /// Normalize the stimuli in the [0.0;1.0] or [-1.0;1.0], depending on the signess,
+    /// if the results of all the transformations of the StimuliProvider are integers,
+    /// do nothing otherwise. The stimuli are normalized by adding extra transformations.
+    /// 
+    /// 'envCvDepth' is the OpenCV depth of the inputs coming from the environment.
+    bool normalizeIntegersStimuli(int envCvDepth);
+
     void logTransformations(const std::string& fileName) const;
 
 

commit db703d26eb23c955f23d069cd073916a4a2b94c6

Fixed bug introduced in commit 77fce58ec7c949bbf5b64dc4fd3c6ff9da1448a3 when mDataSignedMapping is true in StimuliProvider

include/utils/Utils.hpp
@@ -311,7 +311,8 @@ namespace Utils {
     void colorReduce(cv::Mat& img, unsigned int nbColors);
     void colorDiscretize(cv::Mat& img, unsigned int nbLevels);
     std::string cvMatDepthToString(int depth);
-    double cvMatDepthUnityValue(int depth);
+    std::pair<double, double> cvMatDepthUnityValue(int depth,
+                                                   bool signedMapping = false);
 
     std::string searchAndReplace(const std::string& value,
                                  const std::string& search,

src/Database/Database.cpp
@@ -1776,8 +1776,8 @@ cv::Mat N2D2::Database::loadStimulusData(StimulusID id)
         cv::Mat dataConverted;
         data.convertTo(dataConverted,
                        mStimuliDepth,
-                       Utils::cvMatDepthUnityValue(mStimuliDepth)
-                       / Utils::cvMatDepthUnityValue(data.depth()));
+                       Utils::cvMatDepthUnityValue(mStimuliDepth).second
+                       / Utils::cvMatDepthUnityValue(data.depth()).second);
         data = dataConverted;
     }
 

src/StimuliProvider.cpp
@@ -315,12 +315,14 @@ bool N2D2::StimuliProvider::normalizeIntegersStimuli(int envCvDepth) {
 
 
         if(mChannelsTransformations.empty()) {
-            const double depthUnityValue = Utils::cvMatDepthUnityValue(depthSet);
-            if(depthUnityValue != 1.0) {
+            const std::pair<double, double> depthUnityValue
+                = Utils::cvMatDepthUnityValue(depthSet, mDataSignedMapping);
+            if(depthUnityValue.second != 1.0) {
                 normalizationOccured = true;
 
                 addOnTheFlyTransformation(
-                    RangeAffineTransformation(RangeAffineTransformation::Divides, depthUnityValue), 
+                    RangeAffineTransformation(RangeAffineTransformation::Plus, depthUnityValue.first,
+                                              RangeAffineTransformation::Divides, depthUnityValue.second), 
                     mDatabase.getStimuliSetMask(stimuliSet)
                 );
             }
@@ -332,13 +334,15 @@ bool N2D2::StimuliProvider::normalizeIntegersStimuli(int envCvDepth) {
                 chDepthSet = mChannelsTransformations[ch](stimuliSet).onTheFly.getOutputsDepth(chDepthSet);
 
 
-                const double depthUnityValue = Utils::cvMatDepthUnityValue(chDepthSet);
-                if(depthUnityValue != 1.0) {
+                const std::pair<double, double> depthUnityValue
+                    = Utils::cvMatDepthUnityValue(chDepthSet, mDataSignedMapping);
+                if(depthUnityValue.second != 1.0) {
                     normalizationOccured = true;
 
                     addChannelOnTheFlyTransformation(
                         ch,
-                        RangeAffineTransformation(RangeAffineTransformation::Divides, depthUnityValue), 
+                        RangeAffineTransformation(RangeAffineTransformation::Plus, depthUnityValue.first,
+                                                RangeAffineTransformation::Divides, depthUnityValue.second), 
                         mDatabase.getStimuliSetMask(stimuliSet)
                     );
                 }

src/utils/Utils.cpp
@@ -281,23 +281,40 @@ std::string N2D2::Utils::cvMatDepthToString(int depth)
     }
 }
 
-double N2D2::Utils::cvMatDepthUnityValue(int depth)
+std::pair<double, double> N2D2::Utils::cvMatDepthUnityValue(int depth,
+                                                            bool signedMapping)
 {
     switch (depth) {
     case CV_8U:
-        return std::numeric_limits<unsigned char>::max();
+        return (signedMapping)
+            ? std::pair<double, double>(std::numeric_limits<char>::min(),
+                                        -std::numeric_limits<char>::min())
+            : std::pair<double, double>(0.0, std::numeric_limits<unsigned char>::max());
     case CV_8S:
-        return std::numeric_limits<char>::max();
+        return (signedMapping)
+            // Note: we replicate the behavior of Tensor<T>::convert()
+            // In this case, the first value is not 0!
+            ? std::pair<double, double>(std::numeric_limits<char>::min(),
+                                        -std::numeric_limits<char>::min())
+            : std::pair<double, double>(0.0, std::numeric_limits<char>::max());
     case CV_16U:
-        return std::numeric_limits<unsigned short>::max();
+        return (signedMapping)
+            ? std::pair<double, double>(std::numeric_limits<short>::min(),
+                                        -std::numeric_limits<short>::min())
+            : std::pair<double, double>(0.0, std::numeric_limits<unsigned short>::max());
     case CV_16S:
-        return std::numeric_limits<short>::max();
+        return (signedMapping)
+            // Note: we replicate the behavior of Tensor<T>::convert()
+            // In this case, the first value is not 0!
+            ? std::pair<double, double>(std::numeric_limits<short>::min(),
+                                        -std::numeric_limits<short>::min())
+            : std::pair<double, double>(0.0, std::numeric_limits<short>::max());
     case CV_32S:
-        return std::numeric_limits<int>::max();
+        return std::pair<double, double>(0.0, std::numeric_limits<int>::max());
     case CV_32F:
-        return 1.0;
+        return std::pair<double, double>(0.0, 1.0);
     case CV_64F:
-        return 1.0;
+        return std::pair<double, double>(0.0, 1.0);
     default:
         throw std::runtime_error(
             "Utils::cvMatDepthUnityValue(): unknown cv::Mat depth");