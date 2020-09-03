CEA-LIST/N2D2

commit 3e6cca02677fbe68c5a73c2c815e98b659eefb55

Fixed possible precision issue

src/DeepNetQuantization.cpp
@@ -376,8 +376,11 @@ std::unordered_map<std::string, long double> N2D2::DeepNetQuantization::quantize
                 biasScaling *= wScalingCell;
             }
 
-
-            cell->processFreeParameters([&](Float_T b) { return b*(bQuantScaling/biasScaling); }, 
+            // Don't divide by biasScaling here, because it could lead to a loss 
+            // of precision because bias is stored as float, not long double
+            // This step has been merged in quantizeActivations()
+            // Original formula: b*(bQuantScaling/biasScaling)
+            cell->processFreeParameters([&](Float_T b) { return b*(bQuantScaling); }, 
                                         Cell::Additive);
             biasScalings[cell->getName()] = biasScaling;
 
@@ -434,9 +437,10 @@ std::unordered_map<std::string, long double> N2D2::DeepNetQuantization::quantize
                     cell->processFreeParametersPerOutput([&](Float_T w) { 
                                                             return w*(wQuantScaling/wScalingCellOutput); 
                                                          }, output, Cell::Multiplicative);
+                    // Don't divide by biasScaling here
                     cell->processFreeParametersPerOutput([&](Float_T b) { 
                                                              return b*(bQuantScaling/
-                                                                       (biasScaling*wScalingCellOutput)); 
+                                                                       (wScalingCellOutput)); 
                                                          }, output, Cell::Additive);
 
                     scalingPerOutput[output] = wScalingCellOutput/wScalingCell;
@@ -542,14 +546,16 @@ void N2D2::DeepNetQuantization::quantizeActivations(
                                          cell->getType() + "' is not supported yet.");
             }
 
+            const long double biasScaling = biasScalings.at(cell->getName());
+
 #ifdef VERBOSE_QUANT
             std::cout << "  - " << cell->getName() << ": "
                 << "prev=" << prevActivationScaling
                 << ", act=" << activationScaling
-                << ", bias=" << biasScalings.at(cell->getName()) << std::endl;
+                << ", bias=" << biasScaling << std::endl;
 #endif
 
-            activationScaling /= biasScalings.at(cell->getName());
+            activationScaling /= biasScaling;
             activationScaling = (activationScaling == 0.0)?1.0:activationScaling;
 
             if(cell->getType() == ElemWiseCell::Type
@@ -561,7 +567,7 @@ void N2D2::DeepNetQuantization::quantizeActivations(
 
             activationScalings[cell->getName()] = activationScaling;
 
-            cell->processFreeParameters([&](Float_T d) { return d/prevActivationScaling; },
+            cell->processFreeParameters([&](Float_T d) { return d/(biasScaling*prevActivationScaling); },
                                         Cell::Additive);
                                         
 
@@ -580,7 +586,7 @@ void N2D2::DeepNetQuantization::quantizeActivations(
             mDeepNet.addCellAfter(scalingCell, cell);
 
             activationScalings[scalingCell->getName()] = activationScalings[cell->getName()];
-            biasScalings[scalingCell->getName()] = biasScalings[cell->getName()];
+            biasScalings[scalingCell->getName()] = biasScaling;
 
 #ifdef VERBOSE_QUANT
             std::cout << "      quant=" << actQuantScaling
@@ -602,7 +608,6 @@ double N2D2::DeepNetQuantization::getActivationQuantizationScaling(const Cell& c
 
     const Cell_Frame_Top& cellFrame = dynamic_cast<const Cell_Frame_Top&>(cell);
     const std::shared_ptr<Activation>& activation = cellFrame.getActivation();
-
 
     if(!activation || cell.getType() == ElemWiseCell::Type) {
         return 1.0;

commit 6dd2cda5ed6bb93067883b62c4b8dae594f640db

Fixed per output quantization (bug introduced in 3e6cca02677fbe68c5a73c2c815e98b659eefb55) and corresponding unit test

src/DeepNetQuantization.cpp
@@ -486,11 +486,7 @@ std::unordered_map<std::string, long double> N2D2::DeepNetQuantization::quantize
                 biasScaling *= wScalingCell;
             }
 
-            // Don't divide by biasScaling here, because it could lead to a loss 
-            // of precision because bias is stored as float, not long double
-            // This step has been merged in quantizeActivations()
-            // Original formula: b*(bQuantScaling/biasScaling)
-            cell->processFreeParameters([&](Float_T b) { return b*(bQuantScaling); }, 
+            cell->processFreeParameters([&](Float_T b) { return b*(bQuantScaling/biasScaling); }, 
                                         Cell::Additive);
             biasScalings[cell->getName()] = biasScaling;
 
@@ -547,10 +543,9 @@ std::unordered_map<std::string, long double> N2D2::DeepNetQuantization::quantize
                     cell->processFreeParametersPerOutput([&](Float_T w) { 
                                                             return w*(wQuantScaling/wScalingCellOutput); 
                                                          }, output, Cell::Multiplicative);
-                    // Don't divide by biasScaling here
                     cell->processFreeParametersPerOutput([&](Float_T b) { 
                                                              return b*(bQuantScaling/
-                                                                       (wScalingCellOutput)); 
+                                                                       (biasScaling*wScalingCellOutput)); 
                                                          }, output, Cell::Additive);
 
                     scalingPerOutput[output] = wScalingCellOutput/wScalingCell;
@@ -670,7 +665,7 @@ void N2D2::DeepNetQuantization::quantizeActivations(
 
             activationScalings[cell->getName()] = activationScaling;
 
-            cell->processFreeParameters([&](Float_T d) { return d/(biasScaling*prevActivationScaling); },
+            cell->processFreeParameters([&](Float_T d) { return d/prevActivationScaling; },
                                         Cell::Additive);
                                         
 

tests/class_DeepNetQuantization.cpp
@@ -156,7 +156,7 @@ TEST_DATASET(DeepNetQuantization, quantization,
                     true, false, 500, 8, 
                     ClippingMode::KL_DIVERGENCE, ScalingMode::FLOAT_MULT,
                     96.5999, 96.6667),
-    
+
     std::make_tuple("tests_data/mnist_model/model.ini", "tests_data/mnist_model/weights", 
                     true, true, 500, 8, 
                     ClippingMode::NONE, ScalingMode::FLOAT_MULT,
@@ -165,15 +165,15 @@ TEST_DATASET(DeepNetQuantization, quantization,
     std::make_tuple("tests_data/mnist_multibranch_model/model.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::NONE, ScalingMode::FLOAT_MULT,
-                    98.5999, 98.6000),
+                    98.5999, 98.5334),
     std::make_tuple("tests_data/mnist_multibranch_model/model.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::NONE, ScalingMode::FIXED_MULT,
-                    98.5999, 98.6000),
+                    98.5999, 98.5334),
     std::make_tuple("tests_data/mnist_multibranch_model/model.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::NONE, ScalingMode::SINGLE_SHIFT,
-                    98.5999, 98.2667),
+                    98.5999, 98.4667),
     std::make_tuple("tests_data/mnist_multibranch_model/model.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::NONE, ScalingMode::DOUBLE_SHIFT,
@@ -185,12 +185,12 @@ TEST_DATASET(DeepNetQuantization, quantization,
     std::make_tuple("tests_data/mnist_multibranch_model/model.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::KL_DIVERGENCE, ScalingMode::FLOAT_MULT,
-                    98.5999, 98.5334),
-    
+                    98.5999, 98.6000),
+
     std::make_tuple("tests_data/mnist_multibranch_model/model.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, true, 500, 8, 
                     ClippingMode::NONE, ScalingMode::FLOAT_MULT,
-                    98.5999, 98.4667)
+                    98.5999, 98.6000)
 )
 {
     REQUIRED(UnitTest::DirExists(N2D2_DATA("mnist")));
@@ -257,15 +257,15 @@ TEST_DATASET(DeepNetQuantization, quantization_CUDA,
     std::make_tuple("tests_data/mnist_multibranch_model/model_CUDA.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::NONE, ScalingMode::FLOAT_MULT,
-                    98.5999, 98.6000),
+                    98.5999, 98.5334),
     std::make_tuple("tests_data/mnist_multibranch_model/model_CUDA.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::NONE, ScalingMode::FIXED_MULT,
-                    98.5999, 98.6000),
+                    98.5999, 98.5334),
     std::make_tuple("tests_data/mnist_multibranch_model/model_CUDA.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::NONE, ScalingMode::SINGLE_SHIFT,
-                    98.5999, 98.2667),
+                    98.5999, 98.4667),
     std::make_tuple("tests_data/mnist_multibranch_model/model_CUDA.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::NONE, ScalingMode::DOUBLE_SHIFT,
@@ -277,12 +277,12 @@ TEST_DATASET(DeepNetQuantization, quantization_CUDA,
     std::make_tuple("tests_data/mnist_multibranch_model/model_CUDA.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, false, 500, 8, 
                     ClippingMode::KL_DIVERGENCE, ScalingMode::FLOAT_MULT,
-                    98.5999, 98.5334),
+                    98.5999, 98.6000),
     
     std::make_tuple("tests_data/mnist_multibranch_model/model_CUDA.ini", "tests_data/mnist_multibranch_model/weights", 
                     true, true, 500, 8, 
                     ClippingMode::NONE, ScalingMode::FLOAT_MULT,
-                    98.5999, 98.4667)
+                    98.5999, 98.6000)
 )
 {
     REQUIRED(UnitTest::DirExists(N2D2_DATA("mnist")));