e989508e0fa2b48d7a38b47ace644085451c7843
Olivier BICHLER - DRT/LIST/DACLE/SCSN/LCE <olivier.bichler@cea.fr>
2021-01-07 18:20:47 +0100

    [CPP Export] Fixed bug

--- export/CPP/src/cpp_utils.cpp
+++ export/CPP/src/cpp_utils.cpp
@@ -135,7 +135,7 @@ void envRead(const std::string& fileName,
 #elif NB_BITS > 32 && NB_BITS < 64
     long long int inputsFixed[size];
 #endif
-    nbRead = fread(inputsFixed, sizeof(inputsFixed[0]), nbChannels, stimuli);
+    nbRead = fread(inputsFixed, sizeof(inputsFixed[0]), size, stimuli);
 
     for (unsigned int i = 0; i < size; ++i)
         data[i] = (DATA_T)inputsFixed[i];
