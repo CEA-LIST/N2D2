2ad2be9606e0bdfdd0019b9fd7a2bb7eae7e28f0
IKucher <Inna.KUCHER@cea.fr>
2021-01-14 13:17:59 +0100

    [QuantTest] BN fusion unit test

--- tests/Cell/class_ConvCell_QuantizerSAT_BNFusion_Frame_CUDA.cpp
+++ tests/Cell/class_ConvCell_QuantizerSAT_BNFusion_Frame_CUDA.cpp
@@ -98,11 +98,11 @@ TEST_DATASET(ConvCell_QuantizerSAT_BNFusion_Frame_CUDA_float,
              )
 {
 
-    std::cout<<"float :: check BN fusion with conv when using SAT quantizer"<<std::endl;
+    std::cout<<"BN fusion with conv and SAT quantizer"<<std::endl;
 
     bool doQuant = true;
 
-    CudaContext::setDevice(0);
+    CudaContext::setDevice(3);
     const unsigned int nbOutputs_conv1 = 1;
     const unsigned int nbOutputs_conv2 = 4;
     const unsigned int nbChannels = 1;
@@ -119,6 +119,11 @@ TEST_DATASET(ConvCell_QuantizerSAT_BNFusion_Frame_CUDA_float,
     ASSERT_EQUALS(in.dimZ(), 1U);
     ASSERT_EQUALS(in.dimX(), channelsWidth);
     ASSERT_EQUALS(in.dimY(), channelsHeight);
+
+    Tensor<Float_T> in_fused;
+    in_fused.resize({in.dimX(),in.dimY(),in.dimZ(),in.dimB()},0.0);
+    //std::cout << "in_fused:: X = " << in_fused.dimX() << " , Y = " << in_fused.dimY() << " , Z = " << in_fused.dimZ() << " , B = " << in_fused.dimB() << std::endl;
+
     ///
     //fill input image
     float input_tmp = 0.0f;
@@ -189,15 +194,17 @@ TEST_DATASET(ConvCell_QuantizerSAT_BNFusion_Frame_CUDA_float,
                         if(x==3 && y==4) input_tmp = 56.0f;
                         if(x==4 && y==4) input_tmp = 1.0f;
                     }
-
+                    in_fused(x, y, z, b) = input_tmp;
                     in(x, y, z, b) = input_tmp/255.0f;
-                    //std::cout  << "b, z, y, x = " << b << ", " << z << ", " << y << ", " << x << ", input = " << in(x, y, z, b) << std::endl;
-                }
+                    //std::cout  << "b, z, y, x = " << b << ", " << z << ", " << y << ", " << x << ", input = " << in(x, y, z, b) << " , in_fused = " << in_fused(x, y, z, b) << std::endl;
+                } 
             }
         }
     }
 
+    std::cout << "********************SET_INPUT_0_1********************" << std::endl; 
     std::cout << "[Input]\n" << in << std::endl;
+    std::cout << "********************SET_INPUT_0_1_END********************\n\n" << std::endl; 
 
     ConvCell_QuantizerSAT_BNFusion_Frame_CUDA_Test<float> conv1(dn, "conv1",
         std::vector<unsigned int>({kernelWidth, kernelHeight}),
@@ -208,6 +215,16 @@ TEST_DATASET(ConvCell_QuantizerSAT_BNFusion_Frame_CUDA_float,
         std::vector<unsigned int>({1U, 1U}),
         std::shared_ptr<Activation>());
     conv1.setParameter("NoBias", true);
+
+    ConvCell_QuantizerSAT_BNFusion_Frame_CUDA_Test<float> conv1_fused(dn, "conv1_fused",
+        std::vector<unsigned int>({kernelWidth, kernelHeight}),
+        nbOutputs_conv1,
+        std::vector<unsigned int>({subSampleX, subSampleY}),
+        std::vector<unsigned int>({strideX, strideY}),
+        std::vector<int>({(int)paddingX, (int)paddingY}),
+        std::vector<unsigned int>({1U, 1U}),
+        std::shared_ptr<Activation>());
+    conv1_fused.setParameter("NoBias", false);
 
     BatchNormCell_Frame_CUDA<float> bn1(dn, "bn1", nbOutputs_conv1, std::shared_ptr<Activation>());
 
@@ -328,57 +345,205 @@ TEST_DATASET(ConvCell_QuantizerSAT_BNFusion_Frame_CUDA_float,
         bn1.setBias(output, bias);
         bn1.setMean(output, mean);
         bn1.setVariance(output, variance);
+        std::cout << "********************BN_PARAMS********************" << std::endl; 
         std::cout << "bn1 :: scale = " << scale << " , bias = " << bias << " , mean = " << mean << " , variance = " << variance << std::endl;
+        std::cout << "********************BN_PARAMS_END********************\n\n" << std::endl; 
     }
-    
-    for(unsigned int iter_index = 0; iter_index < 1; ++iter_index){
 
-        std::cout << "iteration #" << iter_index << std::endl;
-        std::cout << "===============================================================" << std::endl;
+    conv1.propagate(true);
+    bn1.propagate(true);
+    conv2.propagate(true);
 
-        std::cout << "******************PROPAGATE*******************\n\n\n" << std::endl;
+    /*
+    conv1.getOutputs().synchronizeDToH();
+    const Tensor<float>& out_conv1 = tensor_cast<float>(conv1.getOutputs());
+    std::cout << "[Conv1][Outputs]" << std::endl;
+    std::cout << out_conv1 << std::endl;
+    conv1.getOutputs().synchronizeHToD();
 
-        conv1.propagate(true);
-        bn1.propagate(true);
-        conv2.propagate(true);
+    bn1.getOutputs().synchronizeDToH();
+    const Tensor<float>& out_bn1 = tensor_cast<float>(bn1.getOutputs());
+    std::cout << "[BN1][Outputs]" << std::endl;
+    std::cout << out_bn1 << std::endl;
+    bn1.getOutputs().synchronizeHToD();
+    */
 
-        conv1.getOutputs().synchronizeDToH();
-        const Tensor<float>& out_conv1 = tensor_cast<float>(conv1.getOutputs());
-        std::cout << "[Conv1][Outputs]" << std::endl;
-        std::cout << out_conv1 << std::endl;
-        conv1.getOutputs().synchronizeHToD();
+    std::cout << "********************CONV2_QUANT_INPUT********************" << std::endl; 
+    quant2.getQuantizedActivations(0).synchronizeDToH();
+    const Tensor<float>& quant_act_conv2 = tensor_cast<float>(quant2.getQuantizedActivations(0));
+    std::cout << "[Conv2][Quant Input]" << std::endl;
+    std::cout << quant_act_conv2 << std::endl;
+    quant2.getQuantizedActivations(0).synchronizeHToD();
+    std::cout << "********************CONV2_QUANT_INPUT_END********************\n\n" << std::endl;
 
-        bn1.getOutputs().synchronizeDToH();
-        const Tensor<float>& out_bn1 = tensor_cast<float>(bn1.getOutputs());
-        std::cout << "[BN1][Outputs]" << std::endl;
-        std::cout << out_bn1 << std::endl;
-        bn1.getOutputs().synchronizeHToD();
+    // ===> fuse BN by hands following SAT paper logic in S7.
 
-        quant2.getQuantizedActivations(0).synchronizeDToH();
-        const Tensor<float>& quant_act_conv2 = tensor_cast<float>(quant2.getQuantizedActivations(0));
-        std::cout << "[Conv2][Quant Input]" << std::endl;
-        std::cout << quant_act_conv2 << std::endl;
-        quant2.getQuantizedActivations(0).synchronizeHToD();
+    // a1 and alpha1 are from conv1; a2 and alpha2 are from conv2;
+    // beta and gamma are from bn1
 
-        /*
-        conv2.getOutputs().synchronizeDToH();
-        const Tensor<float>& out_conv2 = tensor_cast<float>(conv2.getOutputs());
-        std::cout << "[Conv2][Outputs]" << std::endl;
-        std::cout << out_conv2 << std::endl;
-        conv2.getOutputs().synchronizeHToD();
-        */
+    // 0. create conv1_quant layer with inputs in range [0,255] and quantized weights from conv1 rescaled to [-127, 128]
+    //set input and init
 
-        // ===> fuse BN by hands following SAT paper logic in S7.
+    std::cout << "********************SET_INPUT_8-BITS********************" << std::endl; 
+    std::cout << "[Input for fused conv]\n" << in_fused << std::endl;
+    conv1_fused.addInput(in_fused, out_diff);
+    conv1_fused.initialize();
+    std::cout << "********************SET_INPUT_8-BITS_END********************\n\n" << std::endl; 
 
-        // a1 and alpha1 are from conv1; a2 and alpha2 are from conv2;
-        // 0. create conv1_quant layer with inputs in range [0,255] and quantized weights from conv1 rescaled to [-127, 128]
-        // 1. get BN gamma and beta (as done in DeepNet.cpp), these parameters are "-per output", for now 1 output in conv1
-        // 2. add bias : beta/gamma *  a1/alpha1
-        // 3. clip : [0, alpha2/gamma * a1/alpha1]
-        // 4. scale : alpha1/a1 * a2/alpha2 * gamma
-        // 5. round
-        // to compare the result after "round" with quant_act_conv2 :: /255 and * alpha2/a2
+    //set weights for conv1_fused : quantized weights are in the quantizer of conv1
+
+    std::cout << "********************SET_WEIGHTS_8-BITS********************" << std::endl; 
+    quant1.getQuantizedWeights(0).synchronizeDToH();
+    const Tensor<float>& quant_weights_conv1 = tensor_cast<float>(quant1.getQuantizedWeights(0));
+    std::cout << "[Conv1][Quant Weights]" << std::endl;
+    std::cout << quant_weights_conv1 << std::endl;
+    quant1.getQuantizedWeights(0).synchronizeHToD();
+
+    for (unsigned int output = 0; output < nbOutputs_conv1; ++output) {
+        for (unsigned int channel = 0; channel < nbChannels;
+             ++channel) {
+
+            Tensor<float> kernel_rescaled({kernelWidth,
+                                   kernelHeight});
+            
+            for (unsigned int sx = 0; sx < kernelWidth; ++sx) {
+                for (unsigned int sy = 0; sy < kernelHeight; ++sy){
+                    kernel_rescaled(sx, sy) = 0.5*(quant_weights_conv1[output][channel](sx, sy)*range1+1);
+                    std::cout << "conv1_fused :: sx = " << sx << " , sy = " << sy << " , weight = " << quant_weights_conv1[output][channel](sx, sy) << 
+                    " ==> " << kernel_rescaled(sx, sy) << std::endl;
+                }
+            }
+            conv1_fused.setWeight(output, channel, kernel_rescaled);
+        }
     }
+
+    std::cout << "********************SET_WEIGHTS_8-BITS_END********************\n\n" << std::endl; 
+
+    // 2. propagate
+    std::cout << "********************PROPAGATE********************" << std::endl; 
+    conv1_fused.propagate(true);
+
+    conv1_fused.getOutputs().synchronizeDToH();
+    const Tensor<float>& out_conv1_fused_prop = tensor_cast<float>(conv1_fused.getOutputs());
+    std::cout << "[Conv1 Fusion][Output after propagate]" << std::endl;
+    std::cout << out_conv1_fused_prop << std::endl;
+    conv1_fused.getOutputs().synchronizeHToD();
+
+    std::cout << "********************PROPAGATE_END********************\n\n" << std::endl; 
+
+    // 3. get BN gamma and beta (as done in DeepNet.cpp fuseBatchNormWithConv)
+    std::cout << "********************BETA_GAMMA_COMPUTE********************" << std::endl; 
+
+    const bool noBias = conv1.getParameter<bool>("NoBias");
+    const Tensor<float>& bnScales
+        = tensor_cast<float>(*(bn1.getScales()));
+    const Tensor<float>& bnBiases
+        = tensor_cast<float>(*(bn1.getBiases()));
+    const Tensor<float>& bnMeans
+        = tensor_cast<float>(*(bn1.getMeans()));
+    const Tensor<float>& bnVariances
+        = tensor_cast<float>(*(bn1.getVariances()));
+    const double eps = bn1.getParameter<double>("Epsilon");
+
+    float meanVariance = 0.0f;
+    unsigned int count = 0;
+
+    for (std::size_t output = 0; output < nbOutputs_conv1; ++output) {
+        if (bnVariances(output) > 1.0e-12) {
+            meanVariance += bnVariances(output);
+            ++count;
+        }
+        else {
+            std::cout << "    zero-variance " << conv1.getName()
+                << "[" << output << "]" << std::endl;
+        }
+    }
+
+    meanVariance /= count;
+    std::cout << "mean variance = " << meanVariance << std::endl;
+
+    Tensor<float> gamma;
+    gamma.resize({nbOutputs_conv1}, 0.0);
+    Tensor<float> beta;
+    beta.resize({nbOutputs_conv1}, 0.0);
+    Tensor<float> bias_fusion;
+    bias_fusion.resize({nbOutputs_conv1}, 0.0);
+
+    for (unsigned int output = 0; output < nbOutputs_conv1; ++output) {
+        //factor for weights adjustments
+        float factor = bnScales(output)
+                / std::sqrt(eps + ((bnVariances(output) > 1.0e-12)
+                            ? bnVariances(output) : meanVariance));
+        
+        gamma(output) = factor;
+        std::cout << "gamma = " << gamma(output) << std::endl;
+
+        //Biases adjustments
+        Tensor<float> bias;
+        if (noBias)
+            bias.resize({1}, 0.0);
+        else
+            conv1.getBias(output, bias);
+
+        bias(0) = bnBiases(output) + (bias(0) - bnMeans(output)) * factor;
+        beta(output) = bias(0);
+        std::cout << "beta = " << beta(output) << std::endl;
+    }  
+
+    std::cout << "********************BETA_GAMMA_COMPUTE_END********************\n\n" << std::endl; 
+
+
+    //fused output 
+    Tensor<float> conv1_out_fused;
+    conv1_out_fused.resize({conv1_fused.getOutputsWidth(),conv1_fused.getOutputsHeight(),nbOutputs_conv1,batchSize}, 0.0);
+
+    //tensor for comparison with the reference
+    Tensor<float> conv1_out_fused_comp;
+    conv1_out_fused_comp.resize({conv1_fused.getOutputsWidth(),conv1_fused.getOutputsHeight(),nbOutputs_conv1,batchSize}, 0.0);
+
+    std::cout << "********************BN_FUSION********************" << std::endl;
+
+    for (unsigned int batch = 0; batch < batchSize; ++batch) {
+        for (unsigned int output = 0; output < nbOutputs_conv1; ++output) {
+            for (unsigned int oy = 0; oy < conv1_fused.getOutputsHeight(); ++oy) {
+                for (unsigned int ox = 0; ox < conv1_fused.getOutputsWidth(); ++ox) {
+
+                    std::cout << "batch = " << batch << " , x = " << ox << " , y = " << oy << std::endl;
+
+                    float out_tmp = out_conv1_fused_prop(ox, oy, output, batch);
+
+                    // 3. add bias : beta/gamma *  a1/alpha1
+                    bias_fusion(output) = beta(output)/gamma(output) * range1/alpha1;
+                    float out_tmp_bias = out_tmp + bias_fusion(output);
+
+                    // 4. clip the output between : [0, alpha2/gamma * a1/alpha1]
+                    float clipping_factor = alpha2/gamma(output) * range1/alpha1;
+                    float out_tmp_clipped = (out_tmp_bias < 0.0f) ? 0.0f : (out_tmp_bias < clipping_factor) ? out_tmp_bias : clipping_factor;
+
+                    // 5. scale : alpha1/a1 * a2/alpha2 * gamma
+                    float scaling_factor = alpha1/range1 * range2/alpha2 * gamma(output);
+                    float out_tmp_scaled = out_tmp_clipped*scaling_factor;
+
+                    // 6. round
+                    int out_tmp_round = rint(out_tmp_scaled);
+                    conv1_out_fused(ox, oy, output, batch) = out_tmp_round;
+
+                    // to compare the result after "round" with quant_act_conv2 :: rounded/255 and * alpha2/a2
+                    float out_tmp_comp = (float)out_tmp_round/range1 * alpha2/range2;
+                    conv1_out_fused_comp(ox, oy, output, batch) = out_tmp_comp;
+
+                    std::cout << "conv_out = " << out_tmp << " + " << bias_fusion(output) << " = " << out_tmp_bias << std::endl;
+                    std::cout << "clip[0, " << clipping_factor << "] = " << out_tmp_clipped << " * " << scaling_factor  << " = " 
+                    << out_tmp_scaled << " , rounded = " << out_tmp_round << std::endl;
+                    std::cout << " >>> rounded / " << range1 << " * " << alpha2/range2 << " = " << out_tmp_comp << " vs " << quant_act_conv2(ox, oy, output, batch) << std::endl;
+                    std::cout << "*****************************************************" << std::endl;
+
+                }
+            }
+        }
+    }
+
+    std::cout << "********************BN_FUSION_END********************" << std::endl;
 }
 
 
