cef312f3f487d987f729a4c6fbe959cd0657ad9e
Olivier BICHLER - DRT/LIST/DACLE/SCSN/LCE <olivier.bichler@cea.fr>
2021-05-31 17:09:33 +0200

    Fixed doc and test for activation rescaling mode

--- exec/n2d2.cpp
+++ exec/n2d2.cpp
@@ -301,7 +301,7 @@ public:
         actScalingMode = parseScalingMode(
                            opts.parse("-act-rescaling-mode", std::string("Single-shift"), 
                                           "activation scaling mode on export, "
-                                          "can be 'Floating-point', 'Fixed-point', 'Single-shift' "
+                                          "can be 'Floating-point', 'Fixed-point16', 'Fixed-point32', 'Single-shift' "
                                           "or 'Double-shift'"));
         actRescalePerOutput = opts.parse("-act-rescale-per-output", false, 
                                               "rescale activation per output on export");
