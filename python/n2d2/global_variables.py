CEA-LIST/N2D2

commit 2b8cac46486124a729063d4e6414014ff4fce581

Some refactoring of add_input and mapping initialization.

src/Cell/ConvCell_Frame.cpp
@@ -214,9 +214,6 @@ template <class T>
 void N2D2::ConvCell_Frame<T>::initializeParameters(unsigned int nbInputChannels, unsigned int nbInputs)
 {
      // BEGIN: addition to initialize()
-    if (mMapping.empty()) {
-        mMapping.append(Tensor<bool>({getNbOutputs(), nbInputs*nbInputChannels}, true));
-    }
     // TODO: This is only required because getNbChannels() uses the input tensor dimensions to infer the number of input channels. 
     // However, this requires a reinitialization of the input dims which is unsafe
     setInputsDims({nbInputChannels});
@@ -297,6 +294,26 @@ void N2D2::ConvCell_Frame<T>::initializeWeightQuantizer()
     }
 }
 
+template <class T>
+void N2D2::ConvCell_Frame<T>::check_input()
+{
+    if (mInputs.size() != mSharedSynapses.size()) {
+          throw std::runtime_error("mInputs.size() != mSharedSynapses.size() for cell " + mName + 
+          ". Please verify that the number of input tensors given to the cell is"
+          " equal to the number of inputs defined for the cell.");
+    }
+    for (unsigned int k = 0, size = mInputs.size(); k < size; ++k) {
+       if (mInputs[k].dimZ() != mSharedSynapses[k].dimZ()){
+            std::cout << "mInputs.dimZ(): " << mInputs[k].dimZ() << std::endl;
+            std::cout << "mSharedSynapses.dimZ(): " << mSharedSynapses[k].dimZ() << std::endl;
+            std::stringstream ss;
+            ss << "Unmatching dimension Z"
+            " between input and weight " << k << " for cell " + mName;
+            throw std::runtime_error(ss.str());
+        }
+    }
+}
+
 
 template <class T>
 void N2D2::ConvCell_Frame<T>::initializeDataDependent() 
@@ -304,10 +321,7 @@ void N2D2::ConvCell_Frame<T>::initializeDataDependent()
     // NOTE: this is addition to initialize()
     Cell_Frame<T>::initializeDataDependent();
     
-    for (unsigned int k = 0, size = mInputs.size(); k < size; ++k) {
-        if (mInputs[k].size() == 0)
-            throw std::runtime_error("Zero-sized input for ConvCell " + mName);
-    }
+    check_input();
 
 }
 
@@ -347,6 +361,7 @@ void N2D2::ConvCell_Frame<T>::load(const std::string& dirName)
 template <class T>
 void N2D2::ConvCell_Frame<T>::propagate(bool inference)
 {
+    check_input();
     mInputs.synchronizeDBasedToH();
 
     if (mInputs.size() < mSharedSynapses.size()) {
@@ -362,11 +377,6 @@ void N2D2::ConvCell_Frame<T>::propagate(bool inference)
 
     if (mQuantizer) {
         mQuantizer->propagate();
-    }
-
-    // Necessary if no previous call of initializeParameters 
-    if (mMapping.empty()) {
-        mMapping.append(Tensor<bool>({getNbOutputs(), mInputs.size()*mInputs.dimZ()}, true));
     }
 
     for (unsigned int k = 0, size = mInputs.size(); k < size; ++k) {

include/Cell/BatchNormCell_Frame.hpp
@@ -57,6 +57,7 @@ public:
 
     virtual void initialize();
     virtual void initializeParameters(unsigned int nbInputChannels, unsigned int nbInputs);
+    virtual void check_input();
     virtual void initializeDataDependent();
     virtual void propagate(bool inference = false);
     virtual void backPropagate();

include/Cell/BatchNormCell_Frame_CUDA.hpp
@@ -65,6 +65,7 @@ public:
 
     virtual void initialize();
     virtual void initializeParameters(unsigned int nbInputChannels, unsigned int nbInputs);
+    virtual void check_input();
     virtual void initializeDataDependent();
     virtual void propagate(bool inference = false);
     virtual void backPropagate();

include/Cell/Cell.hpp
@@ -283,13 +283,17 @@ public:
     /// Initialize the state of the cell (e.g. weights random initialization)
     virtual void initialize() {};
 
-    virtual void setMapping(const Tensor<bool>& mapping = Tensor<bool>())
+    virtual void setMapping(const Tensor<bool>& mapping)
     {   
         if (!mMapping.empty()){
-            throw std::runtime_error("Cell::initializeMapping(): in "
+            throw std::runtime_error("Cell::setMapping(): in "
                         "cell " + mName + ", mMapping is not empty");
         }
-        if (!mapping.empty()) {
+        if (mapping.empty()) {
+            throw std::runtime_error("Cell::setMapping(): in "
+                        "cell " + mName + ", mapping is empty");
+        }
+        else {
             mMapping.append(mapping);
         }
     };

include/Cell/ConvCell_Frame.hpp
@@ -82,6 +82,7 @@ public:
     virtual void initialize();
     virtual void initializeParameters(unsigned int nbInputChannels, unsigned int nbInputs);
     virtual void initializeWeightQuantizer();
+    virtual void check_input();
     virtual void initializeDataDependent();
     virtual void save(const std::string& dirName) const;
     virtual void load(const std::string& dirName);

include/Cell/ConvCell_Frame_CUDA.hpp
@@ -92,6 +92,7 @@ public:
     virtual void initialize();
     virtual void initializeParameters(unsigned int nbInputChannels, unsigned int nbInputs);
     virtual void initializeWeightQuantizer();
+    virtual void check_input();
     virtual void initializeDataDependent();
     virtual void save(const std::string& dirName) const;
     virtual void load(const std::string& dirName);

include/Cell/DeconvCell_Frame.hpp
@@ -71,6 +71,7 @@ public:
 
     virtual void initialize();
     virtual void initializeParameters(unsigned int nbInputChannels, unsigned int nbInputs);
+    virtual void check_input();
     virtual void initializeDataDependent();
     virtual void propagate(bool inference = false);
     virtual void backPropagate();

include/Cell/DeconvCell_Frame_CUDA.hpp
@@ -79,6 +79,7 @@ public:
 
     virtual void initialize();
     virtual void initializeParameters(unsigned int nbInputChannels, unsigned int nbInputs);
+    virtual void check_input();
     virtual void initializeDataDependent();
     virtual void propagate(bool inference = false);
     virtual void backPropagate();

commit 4cfbd1fb4f970d46e60460cb821cb44bbd8eccfe

Fix a bug with ConCellFrame introduced in 2b8cac46486124a729063d4e6414014ff4fce581.

python/n2d2/global_variables.py
@@ -29,7 +29,7 @@ default_model = 'Frame'
 default_datatype = 'float'
 default_net = N2D2.Network(default_seed, saveSeed=False)
 cuda_compiled = N2D2.cuda_compiled
-n2d2_ip = N2D2.N2D2_IP # True if n2d2-ip is compiled
+n2d2_ip_compiled = N2D2.N2D2_IP 
 
 objects_counter = {}
 

src/Cell/ConvCell_Frame.cpp
@@ -219,7 +219,9 @@ void N2D2::ConvCell_Frame<T>::initializeParameters(unsigned int nbInputChannels,
     setInputsDims({nbInputChannels});
     // END: addition to initialize
 
-
+    if (mMapping.empty()) {
+        mMapping.append(Tensor<bool>({getNbOutputs(), nbInputs*nbInputChannels}, true));
+    }
     if (!mNoBias) {
         if (mBias->empty()) {
             mBias->resize({1, 1, getNbOutputs(), 1});

python/tests/test_pytorch.py
@@ -527,7 +527,6 @@ class test_interop(unittest.TestCase):
 
     def tearDown(self):
         n2d2.global_variables.default_model = "Frame"
-    @unittest.skip("Conv CPU on N2D2 is broken")
     def test_conv_CPU(self):
         print('=== Testing Conv layer CPU ===')
         n2d2.global_variables.default_model = "Frame"