ff62b83735b837c115c7a89f4c345fc7ac476e34
Olivier BICHLER - DRT/LIST/DACLE/SCSN/LCE <olivier.bichler@cea.fr>
2021-09-24 13:53:48 +0200

    [Appveyor] Another try

--- .appveyor/install.cmd
+++ .appveyor/install.cmd
@@ -20,7 +20,12 @@ git clone -q --branch=master https://github.com/google/protobuf.git C:\projects\
 cd C:\projects\protobuf
 mkdir build_cmake
 cd build_cmake
-cmake ..\cmake -A x64 -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL
+cmake ..\cmake -A x64 ^
+  -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL ^
+  -DCMAKE_CONFIGURATION_TYPES="Release" ^
+  -DBUILD_SHARED_LIBS=0 ^
+  -Dprotobuf_BUILD_TESTS=0 ^
+  -Dprotobuf_MSVC_STATIC_RUNTIME=0
 cmake --build . --config Release
 cmake --build . --config Release --target install
 set CMAKE_INCLUDE_PATH=%CMAKE_INCLUDE_PATH%;C:/Program Files (x86)/protobuf/include
