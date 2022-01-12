590e88a82334eae9c4625173e96ecbefe23cba7a
Vincent TEMPLIER <vincent.templier@cea.fr>
2022-01-12 15:31:15 +0100

    Fix miscellaneous warnings during compilation

--- src/Export/CPP/CPP_FcCellExport.cpp
+++ src/Export/CPP/CPP_FcCellExport.cpp
@@ -240,14 +240,15 @@ void N2D2::CPP_FcCellExport::generateHeaderWeightsQAT(const FcCell & cell, std::
     Tensor<Float_T> weight;
     Float_T value;
     uint32_t accumulator = 0;
-    int wCounter = 0;
+    std::size_t wCounter = 0;
 
     // Need it in OHWC order, the order in the weights tensor is OCHW.
     std::size_t iweight = 0;
     for (std::size_t output = 0; output < cell.getNbOutputs(); output++) {
-        std::size_t maxHWC = (cell.getNbChannels()-1)*cell.getChannelsHeight()*cell.getChannelsWidth()
-                    + (cell.getChannelsHeight()-1)*cell.getChannelsWidth()
-                    + (cell.getChannelsWidth()-1);
+        // Not used for now but it will in the upcoming features
+        // std::size_t maxHWC = (cell.getNbChannels()-1)*cell.getChannelsHeight()*cell.getChannelsWidth()
+        //             + (cell.getChannelsHeight()-1)*cell.getChannelsWidth()
+        //             + (cell.getChannelsWidth()-1);
 
         for (std::size_t h = 0; h < cell.getChannelsHeight(); h++) {
             for (std::size_t w = 0; w < cell.getChannelsWidth(); w++) {
