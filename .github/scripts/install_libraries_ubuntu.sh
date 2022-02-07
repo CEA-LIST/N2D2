2ad4db7fc2f30d0cfec07b36ee48f0f38093c690
Vincent TEMPLIER <vincent.templier@cea.fr>
2022-02-07 10:55:08 +0100

    Add scripts to test building on Github CI

---
+++ .github/scripts/install_libraries_ubuntu.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+################################################################################
+#    (C) Copyright 2022 CEA LIST. All Rights Reserved.
+#    Contributor(s): Vincent TEMPLIER (vincent.templier@cea.fr)
+#
+#    This software is governed by the CeCILL-C license under French law and
+#    abiding by the rules of distribution of free software.  You can  use,
+#    modify and/ or redistribute the software under the terms of the CeCILL-C
+#    license as circulated by CEA, CNRS and INRIA at the following URL
+#    "http://www.cecill.info".
+#
+#    As a counterpart to the access to the source code and  rights to copy,
+#    modify and redistribute granted by the license, users are provided only
+#    with a limited warranty  and the software's author,  the holder of the
+#    economic rights,  and the successive licensors  have only  limited
+#    liability.
+#
+#    The fact that you are presently reading this means that you have had
+#    knowledge of the CeCILL-C license and that you accept its terms.
+################################################################################
+
+
+apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    git \
+    wget \
+    gnuplot \
+    libopencv-dev \
+    python-dev \
+    python3-dev \
+    protobuf-compiler \
+    libprotoc-dev \
+    libjsoncpp-dev
