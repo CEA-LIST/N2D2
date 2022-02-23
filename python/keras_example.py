[KerasInterop] Fixing backpropagation unit test

python/keras_example.py
@@ -40,6 +40,7 @@ x_test = x_test.astype("float32") / 255
 x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
 print("x_train shape:", x_train.shape)
+
 print(x_train.shape[0], "train samples")
 print(x_test.shape[0], "test samples")
 
@@ -47,6 +48,7 @@ print(x_test.shape[0], "test samples")
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
+print("y_train shape:", y_train.shape)
 
 """
 ## Build the model
@@ -64,7 +66,7 @@ tf_model = tf.keras.Sequential(
         layers.Dense(num_classes, activation="softmax"),
     ]
 )
-model = keras_interoperability.wrap(tf_model, batch_size=batch_size, for_export=False)
+model = keras_interoperability.wrap(tf_model, batch_size=batch_size, for_export=True)
 
 
 # model.summary() # TODO : Doesn't work anymore ...

python/keras_interoperability/keras_interface.py
@@ -155,11 +155,7 @@ class CustomSequential(keras.Sequential):
             return outputs
 
 
-def wrap(tf_model, batch_size, name=None, for_export=True):
-    # Don't let TF optimize these layers
-    for layer in tf_model.layers:
-        layer.trainable = False
-
+def wrap(tf_model, batch_size, name=None, for_export=False):
     inputs_shape = np.array(tf_model.inputs[0].shape)
     inputs_shape[0] = batch_size
     outputs_shape = tf_model.outputs[0].shape
@@ -210,10 +206,13 @@ def wrap(tf_model, batch_size, name=None, for_export=True):
             # ONNX import Softmax with_loss = True supposing we are using a CrossEntropy loss.
             cell.with_loss = False
         if for_export:
-            if isinstance(cell, n2d2.cells.Fc):
-                # Keras add Reshape before FullyConnected layers however N2D2 doesn't need them !
-                if ((previous_cell is not None) and isinstance(previous_cell, n2d2.cells.Reshape)):
+            # Keras add Reshape before FullyConnected layers, which are not exportable.
+            if isinstance(cell, n2d2.cells.Fc) and isinstance(previous_cell, n2d2.cells.Reshape):
+                try:
                     deepNetCell.remove(previous_cell.get_name(), reconnect=True)
+                except RuntimeError as err:
+                    raise RuntimeError(f'N2D2 could not remove the layer "{previous_cell.get_name()}".\n \
+                        If you do not need to export this network, try to set "for_export=False"') from err
         previous_cell = cell
     
     print("N2D2 model : \n", deepNetCell)

python/tests/test_keras.py
@@ -32,6 +32,7 @@ import tensorflow.keras as keras
 class test_keras(unittest.TestCase):
     def setUp(self):
         pass
+
     @unittest.skip("This test is deprecated due to N2D2 not supporting a Pool Layer as an input.")
     def test_propagation(self):
         tf_model = keras.Sequential([
@@ -51,13 +52,8 @@ class test_keras(unittest.TestCase):
             Conv2D(3, kernel_size=(1, 1))
         ])
         self.model = wrap(tf_model, batch_size=5)
-        self.model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"]) # TODO : useless
         self.x = tf.random.uniform([5,1,3,3])
-        print("=====================")
-        print(self.x)
         n2d2_y = self.model.call(self.x)
-        print(self.x)
-        print("=====================")
         tf_y = tf_model.call(self.x)
         print("N2D2 output : ")
         print(n2d2_y)
@@ -66,24 +62,45 @@ class test_keras(unittest.TestCase):
         for predicted, truth in zip(n2d2_y.numpy().flatten(), tf_y.numpy().flatten()):
             self.assertTrue((abs(float(predicted) - float(truth)) < (0.01 * (abs(truth)+ 0.0001))))
 
-    ##################
-    # def test_backpropagation_conv(self):
-    #     self.model = CustomSequential([
-    #         Input(shape=[3, 3, 2]),
-    #         Conv2D(3, kernel_size=(1, 1))
-    #     ], batch_size=5)
-    #     self.model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
-    #     self.model.tf_model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
 
-    #     self.x = tf.random.uniform([25,3,3,2])
-    #     self.y = tf.random.uniform([25,3,3,3])
-    #     self.xn = tf.identity(self.x)
-    #     self.yn = tf.identity(self.y)
+    def test_backpropagation_conv(self):
+        tf_model = keras.Sequential([
+            Input(shape=[3, 3, 2]),
+            Conv2D(3, kernel_size=(1, 1), use_bias=False)
+        ])
 
-    #     self.model.tf_model.fit(x=self.x, y=self.y, batch_size=5)
-    #     self.model.fit(x=self.xn, y=self.yn, batch_size=5)
-          # TODO : Faire un fit et essayer de lancer une prédiction pour voir si on apprend la même chose  
-    ##################
+        self.model = wrap(tf_model, batch_size=5)
+        sgd_opt = tf.keras.optimizers.SGD(
+            learning_rate=0.01, momentum=0.0, nesterov=False, name='SGD')
+        self.model.compile(loss="categorical_crossentropy", optimizer=sgd_opt, metrics=["accuracy"])
+        tf_model.compile(loss="categorical_crossentropy", optimizer=sgd_opt, metrics=["accuracy"])
+
+        self.x = tf.random.uniform([5,3,3,2])
+        self.y = tf.random.uniform([5,3,3,3])
+        self.xn = tf.identity(self.x)
+        self.yn = tf.identity(self.y)
+        # for i in tf_model.layers[0].weights:
+        #     print(i)
+        # for i in self.model._deepnet_cell[0].get_biases():
+        #     print(i)
+        tf_model.fit(x=self.x, y=self.y, batch_size=5, validation_split=0, epochs=1)
+        self.model.fit(x=self.xn, y=self.yn, batch_size=5, validation_split=0, epochs=1)
+        
+        # for i in tf_model.layers[0].weights:
+        #     print(i)
+        # for i in self.model._deepnet_cell[0].get_biases():
+        #     print(i)
+
+        self.x = tf.random.uniform([25,3,3,2])
+        n2d2_y = self.model.call(self.x)
+        tf_y = tf_model.call(self.x)
+        # print("N2D2 output : ")
+        # print(n2d2_y)
+        # print("TF output : ")
+        # print(tf_y)
+        for predicted, truth in zip(n2d2_y.numpy().flatten(), tf_y.numpy().flatten()):
+            self.assertTrue((abs(float(predicted) - float(truth)) < (0.01 * (abs(truth)+ 0.0001))), 
+            f"After training, N2D2 and Keras weights diverge.")
 
     def test_propagation_fc(self):
         tf_model = keras.Sequential([