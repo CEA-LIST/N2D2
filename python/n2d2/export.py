6de05595f991f34823606ae48b2d8f09a92a6b2c
cmoineau <cyril.moineau@cea.fr>
2022-02-24 18:41:38 +0100

    [PythonAPI] Adding provider parameter to calibrate a Network to generate an export. Adding inputs check on export function an docstring

--- python/n2d2/export.py
+++ python/n2d2/export.py
@@ -23,7 +23,7 @@ import N2D2
 def _parse_export_parameters(gen_export=None, nb_bits=8, qat_SAT=False, 
                  export_no_unsigned=False, calibration=0, 
                  export_no_cross_layer_equalization=False, 
-                 wt_clipping_mode="MSE", act_clipping_mode="NONE", 
+                 wt_clipping_mode="NONE", act_clipping_mode="MSE", 
                  act_scaling_mode="FLOAT_MULT", act_quantile_value=0.9999,
                  act_rescale_per_output=False, calibration_reload=False, report=100, 
                  export_nb_stimuli_max= -1, wt_round_mode = "NONE", 
@@ -77,65 +77,122 @@ def _parse_export_parameters(gen_export=None, nb_bits=8, qat_SAT=False,
                  find_lr=find_lr
                 )
 
-def export_c(N2D2_deepnet, nb_bits=8, qat_SAT=False, 
-                 export_no_unsigned=False, calibration=0, 
-                 export_no_cross_layer_equalization=False, 
-                 wt_clipping_mode="MSE", act_clipping_mode="NONE", 
-                 act_scaling_mode="FLOAT_MULT", act_quantile_value=0.9999,
-                 act_rescale_per_output=False, calibration_reload=False, report=100, 
-                 export_nb_stimuli_max= -1, wt_round_mode = "NONE", 
-                 b_round_mode="NONE", c_round_mode="NONE", find_lr=0):
-    parameters = _parse_export_parameters(gen_export="C",nb_bits=nb_bits, 
-                 qat_SAT=qat_SAT, export_no_unsigned=export_no_unsigned, 
-                 calibration=calibration,
-                 export_no_cross_layer_equalization=export_no_cross_layer_equalization, 
-                 wt_clipping_mode=wt_clipping_mode, 
-                 act_clipping_mode=act_clipping_mode, 
-                 act_scaling_mode=act_scaling_mode, 
-                 act_quantile_value=act_quantile_value,
-                 act_rescale_per_output=act_rescale_per_output, 
-                 calibration_reload=calibration_reload, 
-                 report=report, 
-                 export_nb_stimuli_max=export_nb_stimuli_max, 
-                 wt_round_mode=wt_round_mode, 
-                 b_round_mode=b_round_mode, 
-                 c_round_mode=c_round_mode, 
-                 find_lr=find_lr)
+def export_c(N2D2_deepnet, provider=None, **kwargs):
+    """Generate a C export of the neural network.
+        
+    :param nb_bits: Number of bits per weight for exports, default=8
+    :type nb_bits: int, optional
+    :param qat_sat: Fuse a QAT trained with SAT method, default=False
+    :type qat_sat: bool, optional
+    :param export_no_unsigned: If True, disable the use of unsigned data type in integer exports, default=False
+    :type export_no_unsigned: bool, optional
+    :param calibration: The number of stimuli used for the calibration (``0`` = no calibration, ``-1`` = use the full test dataset), default=0
+    :type calibration: int, optional
+    :param export_no_cross_layer_equalization: If True, disable the use of cross layer equalization in integer exports, default=False
+    :type export_no_cross_layer_equalization: bool, optional
+    :param wt_clipping_mode: Weights clipping mode on export, can be ``NONE``, ``MSE`` or ``KL-Diveregence``, default="NONE
+    :type wt_clipping_mode: str, optional
+    :param act_clipping_mode: activation clipping mode on export, can be ``NONE``, ``MSE`` or ``KL-Divergence`` or ``Quantile``, default="MSE"
+    :type act_clipping_mode: str, optional
+    :param act_quantile_value: Quantile value for ``Quantile`` clipping mode, default=0.9999
+    :type act_quantile_value: float, optional
+    :param act_rescale_per_output: If True, rescale activation per output on export, default=False
+    :type act_rescale_per_output: bool, optional
+    :param calibration_reload: If True, reload and reuse the data of a previous calibration, default=False
+    :type calibration_reload: bool, optional
+    :param report: Number of steps between reportings, default=100
+    :type report: int, optional
+    :param export_nb_stimuli_max: Maximum number of stimuli to export (0 = no dataset export, -1 = unlimited), default=-1
+    :type export_nb_stimuli_max: int, optional
+    :param wt_round_mode: Weights clipping mode on export, can be `NONE`,`RINTF`, default="NONE"
+    :type wt_round_mode: str, optional
+    :param b_round_mode: Biases clipping mode on export, can be `NONE`,`RINTF`, default="NONE"
+    :type b_round_mode: str, optional
+    :param c_round_mode: Clip clipping mode on export, can be `NONE`,`RINTF`, default="NONE"
+    :type c_round_mode: str, optional
+    :param find_lr: Find an appropriate learning rate over a number of iterations, default=0
+    :type find_lr: int, optional
+    """
+
+    if "act_scaling_mode" in kwargs and kwargs["act_scaling_mode"]!="SINGLE_SHIFT":
+        raise ValueError("C export only support single shift scaling for activation")
+    else:
+        kwargs["act_scaling_mode"]="SINGLE_SHIFT"
+    parameters = _parse_export_parameters(gen_export="C", **kwargs)
     N2D2_deepnet.initialize()
     if (N2D2_deepnet.getDatabase().getNbStimuli(N2D2.Database.StimuliSet.__members__["Validation"]) > 0):
         N2D2_deepnet.exportNetworkFreeParameters("weights_validation")
     else:
         N2D2_deepnet.exportNetworkFreeParameters("weights")
+
+    if provider is not None:
+        N2D2_provider = provider.N2D2()
+        N2D2_database = N2D2_provider.getDatabase()
+        N2D2_deepnet.setDatabase(N2D2_database)
+        N2D2_deepnet.setStimuliProvider(N2D2_provider)
+    
+    if kwargs["calibration"] != 0 and \
+    N2D2_deepnet.getDatabase().getNbStimuli() == 0:
+        raise ValueError("Cannot calibrate the network with an empty database,\
+             please add a dataprovider to run the calibration.")
+
     N2D2.generateExport(parameters.N2D2(), N2D2_deepnet)
 
-def export_cpp(N2D2_deepnet, nb_bits=8, qat_SAT=False, 
-                 export_no_unsigned=False, calibration=0, 
-                 export_no_cross_layer_equalization=False, 
-                 wt_clipping_mode="MSE", act_clipping_mode="NONE", 
-                 act_scaling_mode="FLOAT_MULT", act_quantile_value=0.9999,
-                 act_rescale_per_output=False, calibration_reload=False, report=100, 
-                 export_nb_stimuli_max= -1, wt_round_mode = "NONE", 
-                 b_round_mode="NONE", c_round_mode="NONE", find_lr=0):
-
-    parameters = _parse_export_parameters(gen_export="CPP",nb_bits=nb_bits, 
-                 qat_SAT=qat_SAT, export_no_unsigned=export_no_unsigned, 
-                 calibration=calibration,
-                 export_no_cross_layer_equalization=export_no_cross_layer_equalization, 
-                 wt_clipping_mode=wt_clipping_mode, 
-                 act_clipping_mode=act_clipping_mode, 
-                 act_scaling_mode=act_scaling_mode, 
-                 act_quantile_value=act_quantile_value,
-                 act_rescale_per_output=act_rescale_per_output, 
-                 calibration_reload=calibration_reload, 
-                 report=report, 
-                 export_nb_stimuli_max=export_nb_stimuli_max, 
-                 wt_round_mode=wt_round_mode, 
-                 b_round_mode=b_round_mode, 
-                 c_round_mode=c_round_mode, 
-                 find_lr=find_lr)
+def export_cpp(N2D2_deepnet, provider=None, **kwargs):
+    """Generate a CPP export of the neural network.
+    
+    :param N2D2_deepnet: Deepnet to export.
+    :type N2D2_deepnet: :py:class:`N2D2.DeepNet`
+    :param provider: Data provider that will be used for calibration, default=None
+    :type provider: :py:class:``n2d2.provider.Provider`, optional
+    :param nb_bits: Number of bits per weight for exports, default=8
+    :type nb_bits: int, optional
+    :param qat_sat: Fuse a QAT trained with SAT method, default=False
+    :type qat_sat: bool, optional
+    :param export_no_unsigned: If True, disable the use of unsigned data type in integer exports, default=False
+    :type export_no_unsigned: bool, optional
+    :param calibration: The number of stimuli used for the calibration (``0`` = no calibration, ``-1`` = use the full test dataset), default=0
+    :type calibration: int, optional
+    :param export_no_cross_layer_equalization: If True, disable the use of cross layer equalization in integer exports, default=False
+    :type export_no_cross_layer_equalization: bool, optional
+    :param wt_clipping_mode: Weights clipping mode on export, can be ``NONE``, ``MSE`` or ``KL-Diveregence``, default="NONE
+    :type wt_clipping_mode: str, optional
+    :param act_clipping_mode: activation clipping mode on export, can be ``NONE``, ``MSE`` or ``KL-Divergence`` or ``Quantile``, default="MSE"
+    :type act_clipping_mode: str, optional
+    :param act_scaling_mode: activation scaling mode on export, can be `NONE`, `FLOAT_MULT`, `FIXED_MULT16`, `SINGLE_SHIFT` or `DOUBLE_SHIFT`, default="FLOAT_MULT"
+    :type act_scaling_mode: str, optional
+    :param act_quantile_value: Quantile value for ``Quantile`` clipping mode, default=0.9999
+    :type act_quantile_value: float, optional
+    :param act_rescale_per_output: If True, rescale activation per output on export, default=False
+    :type act_rescale_per_output: bool, optional
+    :param calibration_reload: If True, reload and reuse the data of a previous calibration, default=False
+    :type calibration_reload: bool, optional
+    :param report: Number of steps between reportings, default=100
+    :type report: int, optional
+    :param export_nb_stimuli_max: Maximum number of stimuli to export (0 = no dataset export, -1 = unlimited), default=-1
+    :type export_nb_stimuli_max: int, optional
+    :param wt_round_mode: Weights clipping mode on export, can be `NONE`,`RINTF`, default="NONE"
+    :type wt_round_mode: str, optional
+    :param b_round_mode: Biases clipping mode on export, can be `NONE`,`RINTF`, default="NONE"
+    :type b_round_mode: str, optional
+    :param c_round_mode: Clip clipping mode on export, can be `NONE`,`RINTF`, default="NONE"
+    :type c_round_mode: str, optional
+    :param find_lr: Find an appropriate learning rate over a number of iterations, default=0
+    :type find_lr: int, optional
+    """
+    parameters = _parse_export_parameters(gen_export="CPP", **kwargs)
     N2D2_deepnet.initialize()
     if (N2D2_deepnet.getDatabase().getNbStimuli(N2D2.Database.StimuliSet.__members__["Validation"]) > 0):
         N2D2_deepnet.exportNetworkFreeParameters("weights_validation")
     else:
         N2D2_deepnet.exportNetworkFreeParameters("weights")
+    if provider is not None:
+        N2D2_provider = provider.N2D2()
+        N2D2_database = N2D2_provider.getDatabase()
+        N2D2_deepnet.setDatabase(N2D2_database)
+        N2D2_deepnet.setStimuliProvider(N2D2_provider)
+    if kwargs["calibration"] != 0 and \
+    N2D2_deepnet.getDatabase().getNbStimuli() == 0:
+        raise ValueError("Cannot calibrate the network with an empty database,\
+             please add a dataprovider to run the calibration.")
     N2D2.generateExport(parameters.N2D2(), N2D2_deepnet)
